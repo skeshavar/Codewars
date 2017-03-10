256a966ff14a52e7ca464ebd50369a5216ee72d3
skeshavar <k7ragav@hotmail.com>
2017-03-10 10:29:04 +0100

    Delete codewars7.py

--- codewars7.py
+++
@@ -1,9 +0,0 @@
-def duplicate_count(test):
-    test = test.lower()
-    chk = []
-    for index, i in enumerate(test):
-        if i in test[index+1:len(test)] and i not in chk:
-            chk.append(i)
-    return len(chk)
-
-print duplicate_count("abcdeaB")

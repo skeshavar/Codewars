e6c08a9720989ca3dffdc952facae046867eda21
skeshavar <k7ragav@hotmail.com>
2017-03-10 10:32:38 +0100

    Delete codewars15.py

--- codewars15.py
+++
@@ -1,8 +0,0 @@
-def first_non_repeating_letter(string):
-    string_new  = string.lower()
-    for index, i in enumerate(string_new):
-        if string_new.count(i) == 1:
-            return string[index]
-
-
-print first_non_repeating_letter('STresS')
